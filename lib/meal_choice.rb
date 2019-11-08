02c7f11149d23fad9c5aa101641fc08858aed244
Ana Robalo <anameciarobalo@gmail.com>
2019-11-08 07:41:30 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,6 +2,8 @@
 
 def meal_choice (veg1, veg2, protein="meat")
   puts "What a nutritious meal!"
-  p "A plate of #{protein} with #{veg1} and #{veg2}."
+  phrase = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts phrase
+  p phrase
 end
 
